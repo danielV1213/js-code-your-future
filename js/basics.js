d6520ef1c0fc7ceac5ef70f6bd6bf2a857efa2b6
Daniel Vélez <d.velez@globant.com>
2025-04-10 00:01:32 -0500

    Initial commit

---
+++ js/basics.js
@@ -0,0 +1,56 @@
+// Variables and Primitive Data Types (Number, String, Undefined, Null, and Boolean)
+// let
+let str = "Code Your Future";
+let number = 8;
+let anotherNumber = 8.5;
+let nullValue = null;
+let booleanValue = false;
+let und;
+
+// Reassignment
+// str = 'JavaScript'
+// console.log(typeof number);
+
+// console.log(11 % 2);
+
+// const
+const name = "Daniel";
+
+// No reassignment
+// name = 'José'
+// console.log(name);
+
+// Basic Functions (declarative)
+// Structure: reserved word 'function' + function name + parameters + return
+function greet() {
+  const greeting = "Good evening";
+  return greeting + " " + name;
+}
+
+// console.log(greet());
+
+// Basic Data Structures (Array, Object)
+// Arrays
+const productArray = ["shoes", "t-shirt", "watch"];
+// Objects
+const shoes = {
+  brand: "Shoes",
+  size: 40,
+  color: "#000",
+};
+
+const tshirt = {
+  brand: "T-Shirt",
+  size: "M",
+  color: "#FFF",
+};
+
+const watch = {
+  brand: "Watch",
+  size: 18,
+  color: "#FFD700",
+};
+
+// productArray = [shoes, tshirt, watch];
+
+// console.log(productArray);
---
+++ js/script.js
@@ -0,0 +1,32 @@
+// DOM Manipulation
+
+// Declaration and assignment of variables
+// Constants for handling the mobile menu
+const menuBtn = document.querySelector(".menu-btn");
+const navbarMenu = document.querySelector(".menu");
+const menuBtnIcon = document.querySelector("#menu-btn-icon");
+
+// Constants for handling the shopping cart
+const cartOpenBtn = document.querySelector(".shopping-cart-open-btn");
+const cartCloseBtn = document.querySelector(".shopping-cart-close-btn");
+const cartOverlay = document.querySelector(".overlay");
+const cart = document.querySelector(".shopping-cart");
+
+// Functions
+
+// Function to toggle the 'active' class on the menu and its icon
+
+
+// Function to show or hide the shopping cart and its background overlay
+
+
+// Use of the JavaScript method "addEventListener" which allows executing a function
+// when the specified event occurs on the obtained HTML element
+// The first argument passed to the function is the event, and the second is the function
+// that will be executed when that event occurs
+
+// addEventListener to open or close the responsive menu
+
+
+// addEventListener to open or close the shopping cart
+
